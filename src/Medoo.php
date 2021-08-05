a1d4cb33d85cd7eb2c5492293a9ef30081f5d9d4
iMordecai <46579683+iMordecai@users.noreply.github.com>
2021-08-05 00:20:14 -0400

    Added support for MySQL NOT REGEXP
    There is already support for REGEXP, this edit adds support for NOT GEREXP.

--- src/Medoo.php
+++ src/Medoo.php
@@ -856,7 +856,7 @@ class Medoo
             $isIndex = is_int($key);
 
             preg_match(
-                '/([\p{L}_][\p{L}\p{N}@$#\-_\.]*)(\[(?<operator>\>\=?|\<\=?|\!|\<\>|\>\<|\!?~|REGEXP)\])?([\p{L}_][\p{L}\p{N}@$#\-_\.]*)?/u',
+                '/([\p{L}_][\p{L}\p{N}@$#\-_\.]*)(\[(?<operator>\>\=?|\<\=?|\!|\<\>|\>\<|\!?~|REGEXP| NOTREGEXP)\])?([\p{L}_][\p{L}\p{N}@$#\-_\.]*)?/u',
                 $isIndex ? $value : $key,
                 $match
             );
@@ -964,6 +964,9 @@ class Medoo
                     }
                 } elseif ($operator === 'REGEXP') {
                     $stack[] = "{$column} REGEXP {$mapKey}";
+                    $map[$mapKey] = [$value, PDO::PARAM_STR];
+                } elseif ($operator === 'NOT REGEXP') {
+                    $stack[] = "{$column} NOT REGEXP {$mapKey}";
                     $map[$mapKey] = [$value, PDO::PARAM_STR];
                 }
 
